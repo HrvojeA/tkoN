dc28d63ee07a147e03b76a773338e31e55169c20
Hrvoje Antunović <Hrvoje Antunovi>
2016-08-10 01:23:29 +0200

    Made homepage and lotta css

---
+++ index.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+
+<body>
+
+
+<div class="row nokt-bg" style="margin:0;">
+    <header role="banner">
+        <nav id="navbar-primary" class="navbar" role="navigation">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse"
+                            data-target="#navbar-primary-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="navbar-primary-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#"><img id="logo-navbar-middle" src="images/logo.png"
+                                             alt="Logo Thing main logo"></a></li>
+                        <li class="active"><a href="#">Naslovnica</a></li>
+                        <li><a href="#">O nama</a></li>
+
+                        <li><a href="#">Donacije</a></li>
+                        <li><a href="#">Kontakt</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+    </header>
+    <nav class="hamby">
+
+        <input id="nav" type="checkbox"/>
+
+        <label for="nav">
+            <strong style="margin-right:10px;">Menu</strong>
+            <b><i></i><i></i><i></i></b>
+        </label>
+
+        <menu>
+            <li><a href="#">Naslovnica</a></li>
+            <li><a href="#">O nama</a></li>
+            <li><a href="#">Donacije</a></li>
+            <li><a href="#">Kontakt</a></li>
+        </menu>
+
+    </nav>
+    <div class="container header-heading">
+        <h1>Trebamo i Vašu pomoć</h1>
+        <h4>Kad činimo dobro, potrebno ga je činiti s radošću.</h4>
+        <div style="width:50%;margin:auto;margin-top:50px; border-top:1px solid white; padding-top:30px;"><p>NOKT (Neki
+                Onima Koji Trebaju) je humanitarna udruga, utemeljena i registrirana od strane djelatnika poduzeća
+                NSoft,
+                koji su na ovaj način htjeli podići svijest o važnosti dobrotvornog rada i pomaganja onima koji su u
+                potrebi.</p>
+        </div>
+        <div style="width:20%; margin:auto; border:1px solid white;"><h4>Više o nama</h4></div>
+    </div>
+</div>
+<div class="row nokt-latest-donations"  >
+
+    <div class="col-xs-12 col-md-6  " style="background:#255d87;padding-bottom:100px;">
+        <div class="col-xs-12 col-md-4"></div>
+        <div class="col-xs-12 col-md-8 pull-right pusher">
+            <div class="col-xs-12">
+                <h2>Zadnje donacije</h2>
+            </div>
+            <div class="col-xs-12">
+                <h3>Donacije udruzi</h3>
+            </div>
+            <div class="col-xs-12 nokt-inv-separator"></div>
+            <div class="col-xs-12 nokt-donation-article">
+                <p>Donacije lorem ipsum, tekst donor. Trump Jesam Nisam.</p>
+            </div>
+            <div class="col-xs-12 nokt-donation-article">
+                <p>Donacije lorem ipsum, tekst donor. Trump Jesam Nisam.</p>
+            </div>
+            <div class="col-xs-12 nokt-donation-article">
+                <p>Donacije lorem ipsum, tekst donor. Trump Jesam Nisam.</p>
+            </div>
+            <div class="col-xs-12 nokt-donation-article">
+                <p>Donacije lorem ipsum, tekst donor. Trump Jesam Nisam.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-md-6  " style="background:#1a4764;padding-bottom:100px;">
+        <div class="col-xs-12 col-md-8 pull-left pusher">
+            <div class="col-xs-12" style="visibility: hidden;">
+                <h2>Zadnje donacije</h2>
+            </div>
+            <div class="col-xs-12">
+                <h3>Donacije potrebitima</h3>
+            </div>
+            <div class="col-xs-12 nokt-inv-separator"></div>
+            <div class="col-xs-12 nokt-donation-article">
+                <p>Donacije lorem ipsum, tekst donor. Trump Jesam Nisam.</p>
+            </div>
+            <div class="col-xs-12 nokt-donation-article">
+                <p>Donacije lorem ipsum, tekst donor. Trump Jesam Nisam.</p>
+            </div>
+            <div class="col-xs-12 nokt-donation-article">
+                <p>Donacije lorem ipsum, tekst donor. Trump Jesam Nisam.</p>
+            </div>
+            <div class="col-xs-12 nokt-donation-article">
+                <p>Donacije lorem ipsum, tekst donor. Trump Jesam Nisam.</p>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-4"></div>
+
+    </div>
+
+</div>
+<div class="row" style="background-color:white; padding-top:50px;">
+    <div class="container">
+        <div class="col-xs-12" style="margin-bottom:15px;">
+            <div class="col-xs-12 col-md-3"><h3>Najnovije vijesti</h3></div>
+            <div class="col-xs-12 col-md-3"><div class="btn btn-primary"></div></div>
+        </div>
+        <div class="col-xs-12 col-md-3 nokt-thumbnail">
+            <div class="thumbnail">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/9/94/Zzyzxrd.350px.jpg" alt = "Generic placeholder thumbnail">
+            </div>
+
+            <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Some sample text.  Some sample text. Some sample text.</p>
+                <p>17.07.2222. </p>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-md-3 nokt-thumbnail">
+            <div class="thumbnail">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/9/94/Zzyzxrd.350px.jpg" alt = "Generic placeholder thumbnail">
+            </div>
+
+            <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Some sample text.  Some sample text. Some sample text.</p>
+                <p>17.07.2222. </p>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-md-3 nokt-thumbnail">
+            <div class="thumbnail">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/9/94/Zzyzxrd.350px.jpg" alt = "Generic placeholder thumbnail">
+            </div>
+
+            <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Some sample text.  Some sample text. Some sample text.</p>
+                <p>17.07.2222. </p>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-md-3 nokt-thumbnail">
+            <div class="thumbnail">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/9/94/Zzyzxrd.350px.jpg" alt = "Generic placeholder thumbnail">
+            </div>
+
+            <div class="caption">
+                <h4>Thumbnail label</h4>
+                <p>Some sample text.  Some sample text. Some sample text.</p>
+                <p>17.07.2222. </p>
+            </div>
+        </div>
+
+</div>
+    </div>
+<div class="row svg" style="padding:80px 0 80px 0;">
+
+    <div class="col-xs-12 col-sm-3"></div>
+    <div class="col-xs-12 col-sm-3">
+        <div id="svg1" class="flip"></div>
+    </div>
+    <div class="col-xs-12 col-sm-3 text">
+        <h2>8232 KM</h2>
+        <h3>Prikupljeno do sada</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div class="col-xs-12 col-sm-3"></div>
+</div>
+<div class="row svg" style=" background: #3580b2; padding:80px 0 80px 0;">
+
+    <div class="col-xs-12 col-sm-3"></div>
+
+    <div class="col-xs-12 col-sm-3 text" style="  ">
+        <h2>9034 KM</h2>
+        <h3>Donirano potrebitima do sada</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div class="col-xs-12 col-sm-3">
+        <div id="svg2" class="flip"></div>
+    </div>
+</div>
+<div class="row svg" style="padding:80px 0 80px 0;">
+
+    <div class="col-xs-12 col-sm-3"></div>
+    <div class="col-xs-12 col-sm-3">
+        <div id="svg3" class="flip">
+        </div>
+
+    </div>
+    <div class="col-xs-12 col-sm-3 text" style="padding-bottom:100px;">
+        <h2>8232 KM</h2>
+        <h3>Prikupljeno do sada</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div class="col-xs-12 col-sm-3"></div>
+</div>
+<div class="row nokt-footer">
+    <div class="container">
+        <div class="col-xs-12 col-md-4  ">
+            <img style="margin-bottom:15px;" src="images/logo.png"/>
+
+            <p>Bleiburških žrtava, bb <br> 88000 Mostar, BiH</p>
+            <p>567897654356789</p>
+            <p>Bleiburških žrtava, bb<br>88000 Mostar, BiH </p>
+        </div>
+        <div class="col-xs-12 col-md-4  nokt-footer-widget">
+            <p>PRIMAJTE VIJESTI IZ UDRUGE</p>
+            <div class="form-group">
+                <input placeholder="Unesite vašu e-mail adresu" type="text" style="height:40px; width:80%;">
+            </div>
+            <div style="width:80%;height:40px; padding:12px 3px 3px 9px; background-color: #3598db;line-height:16px; font-size:16px;"> Pretplati se
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-4  nokt-footer-widget">
+            <p class="nokt-footer-lead">DRUŠTVENE MREŽE</p>
+            <a href="https://www.facebook.com/neki.onima.koji.trebaju/">
+                <div
+                    style="width:80%;height:40px; padding:12px 3px 3px 9px; background-color: #3b5998;line-height:16px; font-size:16px; margin-bottom: 15px;">
+                    Facebook
+                    <i class="fa fa-facebook pull-right" aria-hidden="true"
+                       style="border-left:1px solid white; padding-left:8px; margin-right:17px;"></i>
+                </div>
+            </a>
+            <a href="#">
+                <div
+                    style="width:80%;height:40px; padding:12px 3px 3px 9px; background-color: #dd4b39;line-height:16px; font-size:16px;">
+                    Google+
+                    <i class="fa fa-google-plus pull-right" aria-hidden="true"
+                       style="border-left:1px solid white; padding-left:5px; margin-right:10px;"></i>
+                </div>
+            </a>
+        </div>
+    </div>
+
+</div>
+<script>
+    window.onload = function () {
+
+        bodymovin.loadAnimation({
+            wrapper: document.getElementById('svg3'),
+            animType: 'svg',
+            loop: true,
+            path: 'svg/clanovi/data.json'
+        });
+
+        bodymovin.loadAnimation({
+            wrapper: document.getElementById('svg1'),
+            animType: 'svg',
+            loop: true,
+            path: 'svg/skupljeno/data.json'
+        });
+
+        bodymovin.loadAnimation({
+            wrapper: document.getElementById('svg2'),
+            animType: 'svg',
+            loop: true,
+            path: 'svg/donirano/data.json'
+        });
+    }
+</script>
+<script type="text/javascript" src="svg/bodymovin.js"></script>
+<script src="http://cdn.jsdelivr.net/velocity/1.2.3/velocity.min.js"></script>
+<script src="http://cdn.jsdelivr.net/velocity/1.2.3/velocity.ui.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+
+</html>
